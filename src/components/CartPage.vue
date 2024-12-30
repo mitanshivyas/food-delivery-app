eef9a5014d0b5b861d2c1e5a4da442411f7949c6
Mitanshi Vyas <vyas.mit@northeastern.edu>
2024-12-30 06:59:15 -0600

    Fixed Navigation bar

--- src/components/CartPage.vue
+++ src/components/CartPage.vue
@@ -1,154 +1,177 @@
 <template>
-    <!-- Notification -->
+  <!-- Notification -->
+  <div
+    v-if="showNotification"
+    class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+  >
     <div
-      v-if="showNotification"
-      class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      class="relative bg-green-500 text-white px-6 py-4 rounded shadow-lg w-80 text-center animate-scale-up"
     >
-      <div
-        class="relative bg-green-500 text-white px-6 py-4 rounded shadow-lg w-80 text-center animate-scale-up"
+      <!-- Close Button -->
+      <button
+        @click="showNotification = false"
+        class="absolute top-2 right-2 text-white hover:text-gray-300 text-3xl font-bold"
       >
-        <!-- Close Button -->
-        <button
-          @click="showNotification = false"
-          class="absolute top-2 right-2 text-white hover:text-gray-300 text-3xl font-bold"
-        >
-          &times;
-        </button>
-        <!-- Notification Content -->
-        <h2 class="text-2xl font-bold mb-2">Order Placed!</h2>
-        <p>Your order has been placed successfully.</p>
+        &times;
+      </button>
+      <!-- Notification Content -->
+      <h2 class="text-2xl font-bold mb-2">Order Placed!</h2>
+      <p>Your order has been placed successfully.</p>
+    </div>
+  </div>
+
+  <section class="container mx-auto py-8">
+    <h2 class="text-2xl font-bold text-center mt-20 mb-6">Your Cart</h2>
+    <div v-if="cartItems.length > 0">
+      <div class="grid grid-cols-1 gap-6">
+  <div
+    v-for="item in cartItems"
+    :key="item.id"
+    class="max-w-sm w-full lg:max-w-full lg:flex mx-auto"
+  >
+    <!-- Left Section: Image -->
+    <div
+      class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+      :style="{ backgroundImage: `url(${item.img})` }"
+      :title="item.name"
+    >
+    </div>
+
+    <!-- Right Section: Content -->
+    <div
+      class="max-w-sm w-full lg:max-w-full lg:flex border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+    >
+      <div class="mb-8">
+        <div class="text-gray-900 font-bold text-xl mb-2">{{ item.name }}</div>
+        <p class="text-gray-700 text-base">{{ item.description }}</p>
+        <p class="font-bold">Price: ${{ item.price.toFixed(2) }}</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <!-- Quantity Controls -->
+        <p class="text-sm text-gray-500">
+          Quantity:
+          <button
+            @click="$emit('decrease-quantity', item)"
+            class="bg-gray-300 text-black px-2 rounded"
+          >
+            -
+          </button>
+          <span class="px-2">{{ item.quantity }}</span>
+          <button
+            @click="$emit('increase-quantity', item)"
+            class="bg-gray-300 text-black px-2 rounded"
+          >
+            +
+          </button>
+        </p>
+        <!-- Total Price -->
+        <p class="text-lg font-bold">${{ (item.price * item.quantity).toFixed(2) }}</p>
       </div>
     </div>
-  
-    <section class="container mx-auto py-8">
-      <h2 class="text-2xl font-bold text-center mt-20 mb-6">Your Cart</h2>
-      <div v-if="cartItems.length > 0">
-        <div class="grid grid-cols-1 gap-6">
-          <div
-            v-for="item in cartItems"
-            :key="item.id"
-            class="bg-white p-4 shadow rounded flex justify-between items-center"
-          >
-            <div>
-              <img 
-                :src="item.img"
-                :alt="item.name"
-                class="w-12 h-12 object-cover rounded mr-2"
-              />
-              <h3 class="text-xl font-bold">{{ item.name }}</h3>
-              <p class="text-gray-600">{{ item.description }}</p>
-              <p class="font-bold">Price: ${{ item.price.toFixed(2) }}</p>
-              <p class="text-sm text-gray-500">
-                Quantity:
-                <button @click="$emit('decrease-quantity', item)" class="bg-gray-300 text-black px-2 rounded">-</button>
-                <span class="px-2">{{ item.quantity }}</span>
-                <button @click="$emit('increase-quantity', item)" class="bg-gray-300 text-black px-2 rounded">+</button>
-              </p>
-            </div>
-            <p class="text-lg font-bold">${{ (item.price * item.quantity).toFixed(2) }}</p>
-          </div>
-        </div>
-        <div class="mt-8 text-right">
-          <p class="text-xl font-bold">Total: ${{ total }}</p>
+  </div>
+</div>
+
+
+      <div class="mt-8 text-right">
+        <p class="text-xl font-bold">Total: ${{ total }}</p>
+        <button
+          class="bg-green-500 text-white px-6 py-3 rounded mt-4 hover:bg-green-600"
+          @click="handleCheckout"
+        >
+          Checkout
+        </button>
+        <button
+          class="bg-red-500 text-white px-6 py-3 rounded mt-4 hover:bg-red-600"
+          @click="handleClearCart"
+        >
+          Clear Cart
+        </button>
+      </div>
+    </div>
+    <div v-else class="text-center">
+      <p class="text-lg text-gray-500">Your cart is empty.</p>
+    </div>
+    <div
+      v-if="showConfirmation"
+      class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+    >
+      <div class="bg-white p-6 rounded shadow-lg text-center">
+        <p>Are you sure you want to clear the cart?</p>
+        <div class="mt-4">
           <button
-            class="bg-green-500 text-white px-6 py-3 rounded mt-4 hover:bg-green-600"
-            @click="handleCheckout"
+            @click="confirmClearCart"
+            class="bg-red-500 text-white px-4 py-2 rounded mr-2 p-2 shadow-md hover:shadow-lg transition duration-50 ease-in-out transform hover:scale-110"
           >
-            Checkout
+            Yes
           </button>
           <button
-            class="bg-red-500 text-white px-6 py-3 rounded mt-4 hover:bg-red-600"
-            @click="handleClearCart"
+            @click="cancelClearCart"
+            class="bg-gray-300 px-4 py-2 rounded p-2 shadow-md hover:shadow-lg transition duration-50 ease-in-out transform hover:scale-110"
           >
-            Clear Cart
+            No
           </button>
         </div>
       </div>
-      <div v-else class="text-center">
-        <p class="text-lg text-gray-500">Your cart is empty.</p>
-      </div>
-      <div
-        v-if="showConfirmation"
-        class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
-      >
-        <div class="bg-white p-6 rounded shadow-lg text-center">
-          <p>Are you sure you want to clear the cart?</p>
-          <div class="mt-4">
-            <button
-              @click="confirmClearCart"
-              class="bg-red-500 text-white px-4 py-2 rounded mr-2 p-2 shadow-md hover:shadow-lg transition duration-50 ease-in-out transform hover:scale-110"
-            >
-              Yes
-            </button>
-            <button
-              @click="cancelClearCart"
-              class="bg-gray-300 px-4 py-2 rounded p-2 shadow-md hover:shadow-lg transition duration-50 ease-in-out transform hover:scale-110"
-            >
-              No
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
-  </template>
-  
-  <script>
-  export default {
-    name: "CartPage",
-    props: {
-      cartItems: {
-        type: Array,
-        required: true,
-      },
-      total: {
-        type: String,
-        required: true,
-      },
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "CartPage",
+  props: {
+    cartItems: {
+      type: Array,
+      required: true,
     },
-    data() {
-      return {
-        showNotification: false,
-        showConfirmation: false,
-      };
+    total: {
+      type: String,
+      required: true,
     },
-    methods: {
-      handleCheckout() {
-        this.$emit("checkout"); // Clear the cart
-        this.showNotification = true; // Show notification
-  
-        // Hide the notification after 3 seconds
-        setTimeout(() => {
-          this.showNotification = false;
-        }, 3000);
-      },
-      handleClearCart() {
-        this.showConfirmation = true;
-      },
-      confirmClearCart() {
-        this.$emit("clear-cart");
-        this.showConfirmation = false;
-      },
-      cancelClearCart() {
-        this.showConfirmation = false;
-      },
+  },
+  data() {
+    return {
+      showNotification: false,
+      showConfirmation: false,
+    };
+  },
+  methods: {
+    handleCheckout() {
+      this.$emit("checkout"); // Clear the cart
+      this.showNotification = true; // Show notification
+
+      // Hide the notification after 3 seconds
+      setTimeout(() => {
+        this.showNotification = false;
+      }, 3000);
     },
-  };
-  </script>
-  
-  <style scoped>
-  @keyframes scaleUp {
-    0% {
-      transform: scale(0.5);
-      opacity: 0;
-    }
-    100% {
-      transform: scale(1);
-      opacity: 1;
-    }
+    handleClearCart() {
+      this.showConfirmation = true;
+    },
+    confirmClearCart() {
+      this.$emit("clear-cart");
+      this.showConfirmation = false;
+    },
+    cancelClearCart() {
+      this.showConfirmation = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+@keyframes scaleUp {
+  0% {
+    transform: scale(0.5);
+    opacity: 0;
   }
-  
-  .animate-scale-up {
-    animation: scaleUp 0.3s ease-out forwards;
+  100% {
+    transform: scale(1);
+    opacity: 1;
   }
-  </style>
-  
+}
+
+.animate-scale-up {
+  animation: scaleUp 0.3s ease-out forwards;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,61 +1,99 @@
 <template>
-  <header class="p-4 flex justify-between items-center fixed top-0 w-full z-50">
+  <header
+    class="p-4 flex justify-between items-center fixed top-0 w-full z-50 bg-yellow-400 text-white"
+  >
     <div class="container mx-auto flex justify-between items-center">
-      
+      <!-- Logo Section -->
       <button @click="navigateTo('/')" class="flex items-center space-x-2">
-      
-        <!-- Clickable ChowTown text -->
-        <h1 class="text-xl font-bold hover:text-gray-400">Mitanshi's Mania</h1>
-        <!-- Add food icon -->
-        <font-awesome-icon :icon="['fas', 'utensils']" class="text-pink-500 text-2xl" />
-      </button>
-      
-      <!-- Burger Menu -->
-      <button class="burger-button" @click="toggleMenu">
-        <font-awesome-icon :icon="menuOpen ? 'times' : 'bars'" class="text-xl" />
+        <!-- Brand Name -->
+        <h1 class="text-xl font-bold hover:text-yellow-400">Mitanshi's Mania</h1>
+        <!-- Food Icon -->
+        <font-awesome-icon
+          :icon="['fas', 'utensils']"
+          class="text-pink-500 text-2xl"
+        />
       </button>
 
-      <!-- Navigation -->
-      <nav :class="['md:flex', menuOpen ? 'block' : 'hidden']" class="space-x-4 items-center">
-      <ul class="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
-        <!-- <li><button @click="$emit('navigate', 'home')" class="hover:text-gray-400">Home</button></li>
-        <li><button @click="$emit('navigate', 'menu')" class="hover:text-gray-400">Menu</button></li>
-        <li><button @click="$emit('navigate', 'contact')" class="hover:text-gray-400">Contact</button></li>
-        <li class="relative">
-          <button @click="$emit('navigate', 'cart')" class="relative">
-            <font-awesome-icon icon="shopping-cart" class="text-xl" />
-            <span
-              v-if="cartCount > 0"
-              class="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center"
-            >
-              {{ cartCount }}
-            </span>
-          </button>
-        </li> -->
-        <li><button @click="navigateTo('/')" class="hover:text-gray-400">Home</button></li>
-        <li><button @click="navigateTo('/menu')" class="hover:text-gray-400">Menu</button></li>
-        <li><button @click="navigateTo('/contact')" class="hover:text-gray-400">Contact</button></li>
-        <li class="relative">
-          <button @click="navigateTo('/cart')" class="relative">
-            <font-awesome-icon icon="shopping-cart" class="text-xl" />
-            <span
-              v-if="cartCount > 0"
-              class="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center"
-            >
-              {{ cartCount }}
-            </span>
-          </button>
-        </li>
-      </ul>
-    </nav>
+      <!-- Burger Menu Button (Visible on Mobile) -->
+      <button
+        class="burger-button md:hidden"
+        @click="toggleMenu"
+        aria-label="Toggle navigation"
+      >
+        <font-awesome-icon
+          :icon="menuOpen ? 'times' : 'bars'"
+          class="text-xl"
+        />
+      </button>
 
+      <!-- Navigation Menu -->
+      <nav
+        :class="[
+          'absolute top-full left-0 w-full md:bg-transparent md:relative md:top-0',
+          menuOpen ? 'block' : 'hidden',
+        ]"
+        class="md:flex md:items-center"
+      >
+        <ul
+          class="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 items-center"
+        >
+          <!-- Home Link -->
+          <li>
+            <button
+              @click="navigateTo('/')"
+              class="block text-white hover:text-gray-400 px-4 py-2"
+            >
+              Home
+            </button>
+          </li>
+          <!-- Menu Link -->
+          <li>
+            <button
+              @click="navigateTo('/menu')"
+              class="block text-white hover:text-gray-400 px-4 py-2"
+            >
+              Menu
+            </button>
+          </li>
+          <!-- Contact Link -->
+          <li>
+            <button
+              @click="navigateTo('/contact')"
+              class="block text-white hover:text-gray-400 px-4 py-2"
+            >
+              Contact
+            </button>
+          </li>
+          <!-- Cart Link -->
+          <li class="relative">
+            <button
+              @click="navigateTo('/cart')"
+              class="relative block text-white hover:text-gray-400 px-4 py-2"
+            >
+              <font-awesome-icon icon="shopping-cart" class="text-xl" />
+              <!-- Badge -->
+              <span
+                v-if="cartCount > 0"
+                class="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center"
+              >
+                {{ cartCount }}
+              </span>
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faShoppingCart, faBars, faTimes, faUtensils } from "@fortawesome/free-solid-svg-icons";
+import {
+  faShoppingCart,
+  faBars,
+  faTimes,
+  faUtensils,
+} from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 // Add icons to FontAwesome library
@@ -74,74 +112,71 @@ export default {
   },
   data() {
     return {
-      menuOpen: false,
+      menuOpen: false, // Tracks the state of the burger menu
     };
   },
   methods: {
+    // Programmatically navigate to a route
     navigateTo(route) {
-      this.$router.push(route); // Programmatically navigate to the route
+      this.$router.push(route);
     },
+    // Toggles the burger menu open/close state
     toggleMenu() {
       this.menuOpen = !this.menuOpen;
       console.log("Menu Open State:", this.menuOpen); // Debug menu state
     },
   },
-
 };
 </script>
 
-
 <style scoped>
-
 /* Ensure burger menu button is visible only on small screens */
 .burger-button {
-  display: none; /* Hidden by default */
+  display: none;
 }
 
+/* Display burger button on mobile screens */
 @media (max-width: 768px) {
   .burger-button {
-    display: block; /* Visible on smaller screens */
+    display: block;
   }
 
+  /* Hide navigation menu by default on mobile */
   nav.md\\:flex {
-    display: none; /* Hide the nav flex on smaller screens */
+    display: none;
   }
 }
 
+/* Navigation Menu Styles */
 nav ul {
   display: none; /* Default state */
 }
 
 nav.block ul {
   display: flex; /* Show menu items when menuOpen is true */
-  flex-direction: column;
-  background-color: #1a202c; /* Match the header background */
-  width: 100%; /* Ensure it spans the full width */
-  padding: 1rem 0;
+  flex-direction: column; /* Stack items vertically on mobile */
+  width: 100%; /* Full width for mobile menu */
+  padding: 1rem 0; /* Padding around menu items */
 }
 
-nav ul li {
-  margin: 0.5rem 0; /* Add spacing between items */
-  text-align: center; /* Center-align menu items */
-}
-
+/* Align items horizontally on larger screens */
 @media (min-width: 768px) {
   nav ul {
     display: flex; /* Show inline for larger screens */
     flex-direction: row;
     background-color: transparent; /* Remove background on larger screens */
   }
-  nav ul li {
-    margin: 0; /* Reset margin for horizontal layout */
-  }
 }
 
+nav ul li {
+  margin: 0.5rem 0; /* Spacing between items on mobile */
+  text-align: center; /* Center-align menu items */
+}
+
+/* Style brand name */
 h1 {
-  font-family: 'Playfair Display', serif;
-  font-size: 2.5rem;
-  margin-left: -150px;
-  color: #FF77A9;
-  font-style: bold;
+  font-family: "Playfair Display", serif;
+  font-size: 1.5rem;
+  color: #ff77a9;
 }
-
 </style>
